9336d3eaa4453900c042ad0deb5c61c22e10ee02
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-03-06 16:41:19 -0500

    Complete empty store exercise

--- lib/store.rb
+++ lib/store.rb
@@ -2,6 +2,13 @@ class Store < ActiveRecord::Base
   has_many :employees
 
   validates :name, length: { minimum: 3, allow_nil: false }
-  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  before_destroy do
+    if self.employees.size > 0
+      self.errors.add(:base, "Can't be destroy because of something")
+      throw :abort
+    end
+  end
 
 end
