c20a89f3b91aa5304ab20938a08941f2274ddfed
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-03-06 16:11:54 -0500

    Complete exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3, allow_nil: false }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+
 end
