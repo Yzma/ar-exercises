989abab96d8ebcc40a7850ef5425bd09df6ca34b
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-03-06 14:55:47 -0500

    Complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,26 @@
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+require_relative '../lib/store'
 
-# Your code goes below here ...
+
+# t.column :name, :string
+# t.column :annual_revenue, :integer
+# t.column :mens_apparel, :boolean
+# t.column :womens_apparel, :boolean
+# t.timestamps null: false
+
+# - Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# - Richmond (annual_revenue of 1260000 carries women's apparel only)
+# - Gastown (annual_revenue of 190000 carries men's apparel only)
+
+stores = [
+  Store.new(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true),
+  Store.new(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true),
+  Store.new(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+]
+
+stores.each { |i|
+  i.save
+}
+
+puts "Store count: #{Store.count}"
